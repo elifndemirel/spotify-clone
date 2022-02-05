38c97b537e0b1477b6216a0e076140aa96f6dfe6
Elif Nur Demirel <eliffnurdemirell35@gmail.com>
2022-02-06 00:02:26 +0300

    added download page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Premium from "./pages/Premium";
+import Download from "./pages/Download";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Landing />} />
             <Route exact path="/premium" element={<Premium />} />
+            <Route exact path="/download" element={<Download />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="*" element={<NotFound />} />
           </Routes>
